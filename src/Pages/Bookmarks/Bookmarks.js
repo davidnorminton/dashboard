618023dc783d0111271d8c0e7e9cfedccba81f1b
David Norminton <david.norminton@gmail.com>
2024-07-20 12:40:40 +0100

    List bookmark folders and items into chrome sections

--- src/Pages/Bookmarks/Bookmarks.js
+++ src/Pages/Bookmarks/Bookmarks.js
@@ -1,20 +1,179 @@
 /* global chrome */
 import './css/Bookmarks.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Bookmarks () {
 
-    // useEffect(function () {
-    //     chrome.bookmarks.getChildren(id, function(children) {
-    //         children.forEach(function(bookmark) { 
-    //           console.debug(bookmark.title);
-    //         });
-    //      });
-    // })
+
+    const [ bookmarks, setBookmarks ] = useState([]);
+
+    useEffect(function () {
+
+        const bookmarksFound = [];
+
+        chrome.bookmarks.getTree(function (result) {
+            if (result) {
+                
+                result.forEach(sections => {
+                    
+                    sections.children.forEach(function(topLevelFolder) {
+
+                        const bookmarkSection = {
+                            'title': topLevelFolder.title,
+                            'folders' : [],
+                            'unsorted': []
+                        };
+
+                        topLevelFolder.children.forEach(function (folders) {
+                            console.log(folders)
+                            if (!folders.children) {
+                                bookmarkSection.unsorted.push(folders)
+                            } else {
+                                const tmp = {
+                                    title: folders.title,
+                                    bookmarks: []
+                                }
+
+                                folders.children.forEach(function (child) {
+                                    tmp.bookmarks.push(child)
+                                });
+
+                                bookmarkSection.folders.push(tmp);
+                            }
+                            
+                        });
+
+                        bookmarksFound.push(bookmarkSection);
+                    });
+
+                });
+                console.log(bookmarksFound)
+                setBookmarks(bookmarksFound)
+
+            }
+        })
+
+       // console.log(bookmarks)
+    }, [])
+
+    function getFavicon(url) {
+        const favIconUrl = `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(url)}&size=16`;
+        return favIconUrl
+    }
+
 
     return (
         <div className='page-bookmarks'>
             <h1>Bookmarks</h1>
+            <div className='wrapper'>
+                {
+                    (bookmarks) ? (
+                        bookmarks.map(
+                            (sections) => (
+                                <section>
+                                    <h2>{ sections.title }</h2>
+                                    { /** unsorted bookmark */ }
+                                    {
+                                        (sections.unsorted.length > 0) ? (
+                                            <div>
+                                                <h3>
+                                                    <i class="fa-regular fa-folder bookmark-folder-icon"></i>
+                                                    <span>Unsorted</span>
+                                                </h3>
+                                                <ul className='bookmarks-list'>
+                                                    {
+                                                        sections.unsorted.map(
+                                                            (folder) => (
+                                                                <li>
+                                                                    <a href={ folder.url }>
+                                                                        <img class="favicon" src={ getFavicon(folder.url) } />
+                                                                        <span>
+                                                                        {
+                                                                            (folder.title.length > 40) ?
+                                                                            (
+                                                                                <span>
+                                                                                    { folder.title.substring(0, 40 ) + '...' }
+                                                                                </span>
+
+                                                                            ): (
+                                                                                <span>
+                                                                                    { folder.title }
+                                                                                </span>
+                                                                            )
+                                                                        }                                                                        </span>
+                                                                    </a>
+                                                                </li>
+                                                            )
+                                                        )
+                                                    }
+                                                </ul>
+                                            </div>
+
+                                        ) : ('')
+                                    }
+                                    { /** folders with bookmark */ }
+
+{
+                                        (sections.folders.length > 0) ? (
+                                            <div>
+                                            {
+                                                sections.folders.map(
+                                                    (folder) => (
+                                                        <div>
+                                                            <h3>
+                                                                <i class="fa-regular fa-folder bookmark-folder-icon"></i>
+                                                                <span>{ folder.title }</span>
+                                                            </h3>
+                                                            {
+                                                            (folder.bookmarks) 
+                                                                ? (
+                                                                    <ul className='bookmarks-list'>
+                                                                    {
+                                                                        folder.bookmarks.map(
+                                                                            (folder) => (
+                                                                                <li title={ folder.title }>
+                                                                                    <a href={ folder.url }>
+                                                                                        <img class="favicon" src={ getFavicon(folder.url) } />
+                                                                                        {
+                                                                                            (folder.title.length > 40) ?
+                                                                                            (
+                                                                                                <span>
+                                                                                                    { folder.title.substring(0, 40 ) + '...' }
+                                                                                                </span>
+
+                                                                                            ): (
+                                                                                                <span>
+                                                                                                    { folder.title }
+                                                                                                </span>
+                                                                                            )
+                                                                                        }
+                                                                                    </a>
+                                                                                </li>
+                                                                            )
+                                                                        )
+                                                                    }
+                                                                    </ul>
+                                                                ): ('')
+                                                            }
+
+                                                        </div>
+                                                    )
+                                                )
+                                            }
+                                            </div>
+
+                                        ) : ('')
+                                    }
+
+
+                                </section>
+                            )
+                        )
+                    ) : (
+                        <h2>No bookmarks found </h2>
+                    )
+                }
+            </div>
         </div>
     )
 }
